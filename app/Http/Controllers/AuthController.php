56f9ec6332ab69773084db5f12ef6395052914c2
Bhanwarsa <harsh.rathore14@gmail.com>
2016-03-07 00:02:37 +0530

    Added resource to Questions and Answers Controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,7 +6,26 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
+use App\User;
+
 class AuthController extends Controller
 {
-    //
+    public function login()
+    {
+      return view('auth.login');
+    }
+
+    public function handleLogin(Request $request)
+    {
+      $this->validate($request, User::$login_validation_rules);
+      $data = $request->only('email','password');
+      if(\Auth::attempt($data))
+      {
+          return redirect()->intended('home');
+      }
+      else
+      {
+        return back()->withInput();
+      }
+    }
 }
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,7 +6,28 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
+use App\User;
+
 class UsersController extends Controller
 {
-    //
+    public function create()
+    {
+      return view('users.create');
+    }
+
+    public function store(Request $request)
+    {
+      $this->validate($request, User::$create_validation_rules);
+      $data = $request->only('name','email','password');
+      $data['password'] = bcrypt($data['password']);
+      $user = User::create($data);
+      if($user)
+      {
+        return redirect()->route('home');
+      }
+      else
+      {
+          return back()->withInput();
+      }
+    }
 }
